e57b69f14b593e595f3b730086108516bd338030
iDoc1 <ianardocherty@gmail.com>
2021-05-01 15:57:42 -0700

    repo creation

---
+++ src/main/java/FoodLogMain.java
@@ -0,0 +1,13 @@
+/**
+ * Provides a console interface by which the user can enter information
+ * about food that they ate on specific days for specific meals. The
+ * user can add, delete and modify entries, display food log data, and
+ * enter food-specific details such as calories per serving.
+ */
+
+public class FoodLogMain {
+
+    public static void main(String[] args) {
+
+    }
+}
